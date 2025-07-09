c195d9f74331ab1df3e57b04ba37f9e7b07423a5
StelvioMarques <stelviomarques157@gmail.com>
2025-07-09 17:45:43 +0100

    modulos de temporalidades, classificações e tipo de documentos semi-prontos;
    build do react funcionando lindamente dentro do laravel

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,15 @@ const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'react-assets/[name]-[hash][extname]',
+        chunkFileNames: 'react-assets/[name]-[hash].js',
+        entryFileNames: 'react-assets/[name]-[hash].js',
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
